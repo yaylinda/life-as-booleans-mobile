f70f9168a98ea6f8d05e4ae4a0b6a4edc20a4d96
yaylinda <lindazheng1993@gmail.com>
2023-03-28 21:32:30 -0500

    wip on auth

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
 import {Box, NativeBaseProvider} from "native-base";
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import {SafeAreaView} from "react-native";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
     return (
         <NativeBaseProvider>
-            <Box>Hello world</Box>
+            <SafeAreaView>
+                <Box>Hello world</Box>
+            </SafeAreaView>
         </NativeBaseProvider>
     );
 }
