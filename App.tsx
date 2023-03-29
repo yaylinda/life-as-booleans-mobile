867b9d09b237bab447a1e872ac143423056927d9
yaylinda <lindazheng1993@gmail.com>
2023-03-28 21:45:53 -0500

    wip on auth stack

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
-import {Box, NativeBaseProvider} from "native-base";
+import {Box, NativeBaseProvider, Text} from "native-base";
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
 import {SafeAreaView} from "react-native";
+import {BaseTheme} from "./src/BaseTheme";
+import config from './nativebase.config';
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
     return (
         <NativeBaseProvider>
-            <SafeAreaView>
-                <Box>Hello world</Box>
-            </SafeAreaView>
+            <Text>Hello</Text>
         </NativeBaseProvider>
     );
 }
---
+++ src/BaseTheme.ts
@@ -0,0 +1,7 @@
+import { extendTheme } from 'native-base';
+
+export const BaseTheme = extendTheme({
+    config: {
+        initialColorMode: 'dark',
+    },
+});
