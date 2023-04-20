c3db00b3e9f9fb07f201fb6ac82d149adffdb7cb
yaylinda <lindazheng1993@gmail.com>
2023-04-20 01:58:47 -0500

    need implememnt styles and onpress of list buttons

--- src/screens/today/TrackerList.tsx
+++ src/screens/today/TrackerList.tsx
@@ -1,11 +1,13 @@
+import moment from 'moment';
+import { Button } from 'native-base';
 import React from 'react';
 import Animated from 'react-native-reanimated';
 import Tracker from '../../components/tracker/Tracker';
 import TrackerSingleLine from '../../components/tracker/TrackerSingleLine';
+import { EMPTY_TRACKER } from '../../defaultTrackers';
 import useUserStore from '../../stores/userStore';
 import { UNIT_PX } from '../../styles';
 import type { Tracker as TrackerType } from '../../types';
-import type moment from 'moment';
 
 interface TrackerListProps {
     date: moment.Moment;
@@ -19,18 +21,27 @@ interface IndexedTracker {
 
 const TrackerList = ({ date }: TrackerListProps) => {
 
+    const isToday = moment().isSame(date, 'day');
+
     const { trackers } = useUserStore();
 
-    const indexedTrackers: IndexedTracker[] = React.useMemo(() => (
-        Object.values(trackers).map((tracker: TrackerType, index: number): IndexedTracker => ({
-            index: index,
-            tracker: tracker,
-        }))
-    ), [trackers]);
+    const indexedTrackers: IndexedTracker[] = React.useMemo(() => {
+        const indexTrackers = Object.values(trackers)
+            .map((tracker: TrackerType, index: number): IndexedTracker => ({
+                index: index,
+                tracker: tracker,
+            }));
+
+        return [
+            ...indexTrackers,
+            { tracker: EMPTY_TRACKER(''), index: -1 },
+        ];
+    }, [trackers]);
 
     const renderItem = ({ item }: ({ item: IndexedTracker })) => {
         const { index, tracker } = item;
 
+        // first tracker in list will always be default mood tracker
         if (tracker.isDefaultTracker) {
             return (
                 <Tracker
@@ -41,6 +52,18 @@ const TrackerList = ({ date }: TrackerListProps) => {
             );
         }
 
+        // render buttons - add, and today
+        if ((index < 0) && (isToday)) {
+            return (
+                <Button>Add</Button>
+            );
+        } else if ((index < 0) && (!isToday)) {
+            return (
+                <Button>To to Day</Button>
+            );
+        }
+
+        // render the other non-default trackers
         return (
             <TrackerSingleLine
                 key={`${tracker.id}_${date.valueOf()}`}
