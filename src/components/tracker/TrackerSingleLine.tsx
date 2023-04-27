ae337703fe892c5e1ce382e65efd39db4a6222c9
yaylinda <lindazheng1993@gmail.com>
2023-04-27 00:19:09 -0500

    remove swipe on tracker single line

--- src/components/tracker/TrackerSingleLine.tsx
+++ src/components/tracker/TrackerSingleLine.tsx
@@ -1,28 +1,13 @@
 import { HStack } from 'native-base';
 import React from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
-import { EventRegister } from 'react-native-event-listeners';
-import { PanGestureHandler } from 'react-native-gesture-handler';
-import Animated, {
-    FadeIn,
-    useAnimatedGestureHandler,
-    useAnimatedStyle,
-    useSharedValue,
-    withTiming,
-} from 'react-native-reanimated';
-import { Events } from '../../events';
+import Animated, { FadeIn, useSharedValue } from 'react-native-reanimated';
 import useUserStore from '../../stores/userStore';
-import { BG, UNIT_PX } from '../../styles';
+import { BG } from '../../styles';
 import { withContext } from '../../withContext';
 import { TrackerProvider } from './TrackerContext';
 import TrackerHeader from './TrackerHeader';
-
-import TrackerOptionsRow from './TrackerOptionsRow';
 import TrackerValueSelection from './TrackerValueSelection';
-
 import { useTrackerContext } from './useTrackerContext';
-
-const deviceWidth = Dimensions.get('window').width;
 
 interface TrackerSingleLineProps {
     index: number;
@@ -36,10 +21,6 @@ const TrackerSingleLine = ({ index }: TrackerSingleLineProps) => {
 
     const [value, setValue] = React.useState<string | undefined>('');
 
-    const dragX = useSharedValue(0);
-
-    const threshold = -deviceWidth * 0.3;
-
     React.useEffect(() => {
         const get = async () => {
             const data = await getTrackerData(dayEpoch, tracker.id);
@@ -51,93 +32,30 @@ const TrackerSingleLine = ({ index }: TrackerSingleLineProps) => {
         }
     }, [dayEpoch, getTrackerData, tracker]);
 
-    React.useEffect(() => {
-        const closeOptionsSubscriber = EventRegister.addEventListener(
-            Events.CLOSE_TRACKER_OPTIONS, () => {
-                dragX.value = withTiming(0);
-            });
-
-        return () => {
-            EventRegister.removeEventListener(closeOptionsSubscriber as string);
-        };
-    }, [dragX]);
-
     const onSelectOption = (value: string) => {
         setTrackerData(dayEpoch, tracker.id, value);
         setValue(value);
     };
 
-    const gestureHander = useAnimatedGestureHandler({
-        onActive: (e) => {
-            if (e.translationX > 0) {
-                dragX.value = withTiming(0);
-            }
-            dragX.value = Math.max(threshold, e.translationX);
-        },
-        onEnd: (e) => {
-            if (e.translationX > threshold) {
-                dragX.value = withTiming(0);
-            }
-        },
-    });
-
-    const itemContainerStyle = useAnimatedStyle(() => ({
-        transform: [
-            { translateX: dragX.value },
-        ],
-    }));
-
-    const buttonContainerStyle = useAnimatedStyle(() => {
-        const buttonOpacity = Math.abs(dragX.value / threshold);
-
-        return {
-            opacity: buttonOpacity,
-            transform: [
-                { translateX: deviceWidth + dragX.value },
-            ],
-        };
-    });
-
     return (
-        <PanGestureHandler onGestureEvent={gestureHander}>
-            <Animated.View style={styles.row}>
-                <Animated.View
-                    style={[{}, itemContainerStyle]}
-                    entering={FadeIn.delay(index * 75)}
-                >
-                    <HStack
-                        padding={2}
-                        space={2}
-                        bg={BG}
-                        borderRadius="xl"
-                        alignItems="center"
-                    >
-                        <TrackerHeader />
+        <Animated.View entering={FadeIn.delay(index * 75)}>
+            <HStack
+                padding={2}
+                space={2}
+                bg={BG}
+                borderRadius="xl"
+                alignItems="center"
+                marginBottom={2}
+            >
+                <TrackerHeader />
 
-                        <TrackerValueSelection
-                            selectedValue={value}
-                            onSelect={onSelectOption}
-                        />
-                    </HStack>
-                </Animated.View>
-                <Animated.View style={[styles.options, buttonContainerStyle]}>
-                    <TrackerOptionsRow />
-                </Animated.View>
-            </Animated.View>
-        </PanGestureHandler>
+                <TrackerValueSelection
+                    selectedValue={value}
+                    onSelect={onSelectOption}
+                />
+            </HStack>
+        </Animated.View>
     );
 };
-
-const styles = StyleSheet.create({
-    row: {
-        position: 'relative',
-        marginBottom: 2 * UNIT_PX,
-    },
-    options: {
-        x: deviceWidth,
-        position: 'absolute',
-        height: '100%',
-    },
-});
 
 export default withContext(TrackerSingleLine, TrackerProvider);
